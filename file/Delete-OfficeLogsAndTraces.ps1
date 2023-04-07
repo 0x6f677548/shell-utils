c2133d1198ecbbf4eba4fdf8e7b6420c90fda5fc
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2023-04-07 11:32:51 +0100

    added -day optional parameter

--- file/Delete-OfficeLogsAndTraces.ps1
+++ file/Delete-OfficeLogsAndTraces.ps1
@@ -1,21 +1,33 @@
+param (
+    [Parameter(Mandatory=$false)]
+    [int]$days = 0
+)
 # Description: Delete Office logs and traces
 # Author: 0x6d69636b
+# Usage: Delete-OfficeLogsAndTraces.ps1 -days 1
+
+$limit = (Get-Date).AddDays(-$days)
 
 
 # Delete Office logs and traces from %TEMP%\Diagnostics
 $path = "$env:TEMP\Diagnostics"
+Write-Host "Deleting Office logs and traces from $path"
 
 $folders = Get-ChildItem "$path" -Directory -Name | Where-Object { $_ -in "OUTLOOK", "EXCEL", "WINWORD", "POWERPNT", "ONENOTE", "ACCESS" }
 
 foreach ($folder in $folders) {
     $fullPath = Join-Path "$path" $folder
-    Remove-Item $fullPath -Recurse -Force
+    Write-Host "Deleting files older than $days days in $fullPath :"
+    Get-ChildItem -Path $fullPath -Recurse | Where-Object { $_.LastAccessTime -lt $limit } | Select-Object -Property FullName, LastAccessTime
+    Get-ChildItem -Path $fullPath -Recurse | Where-Object { $_.LastAccessTime -lt $limit } | Remove-Item -Force
 }
 
-$path = "$env:TEMP\Outlook Logging"
+$fullPath = "$env:TEMP\Outlook Logging"
 #check if folder exists
-if ((Test-Path $path)) {
+if ((Test-Path $fullPath)) {
     # Delete outlook logs and traces from %TEMP%\Outlook Logging
-    Remove-Item $path -Recurse -Force
+    Write-Host "Deleting files older than $days days in $fullPath :"
+    Get-ChildItem -Path $fullPath -Recurse | Where-Object { $_.LastAccessTime -lt $limit } | Select-Object -Property FullName, LastAccessTime
+    Get-ChildItem -Path $fullPath -Recurse | Where-Object { $_.LastAccessTime -lt $limit } | Remove-Item -Force
 }
 
--- file/Delete-OldTempFiles.ps1
+++ file/Delete-OldTempFiles.ps1
@@ -1,7 +1,15 @@
-# Description: Delete files older than 7 days in the temp folder
+param(
+    [Parameter(Mandatory=$false)]
+    [int]$days = 7
+)
+# Description: Delete files older than $days days in the temp folder
 # author: 0x6d69636b
+# usage: Delete-OldTempFiles.ps1 -days 7
 
-$limit = (Get-Date).AddDays(-7)
+$limit = (Get-Date).AddDays(-$days)
 $path = "$env:TEMP"
 
+
+Write-Host "Deleting files older than $days days in $path :"
+Get-ChildItem -Path $path -Recurse | Where-Object { $_.LastAccessTime -lt $limit } | Select-Object -Property FullName, LastAccessTime
 Get-ChildItem -Path $path -Recurse | Where-Object { $_.LastAccessTime -lt $limit } | Remove-Item -Force
