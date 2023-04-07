414055c0df246e5a4babdaa5ad42c7ffc9dd0312
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2023-04-07 11:58:33 +0100

    added script to schedule temp files delete

---
+++ file/Schedule-TempFilesDelete.ps1
@@ -0,0 +1,20 @@
+# Description: This script creates a scheduled task to run the Delete-OldTempFiles.ps1 script on logon
+# and the Delete-OfficeLogsAndTraces.ps1 script on logon.
+# It downloads the two PowerShell scripts from the GitHub repository and creates the scheduled task.
+# author: 0x6d69636b
+
+$taskName = "Delete Temp files older than 7 days and Office logs and traces older than 1 day"
+$githubRepoUrl = "https://raw.githubusercontent.com/0x6f677548/shell-utils/main/file"
+$downloadPath = "$env:USERPROFILE"
+$currentUserName = "$env:COMPUTERNAME\$env:USERNAME"
+
+# Download the two PowerShell scripts from the GitHub repository
+Invoke-WebRequest "$githubRepoUrl/Delete-OfficeLogsAndTraces.ps1" -OutFile "$downloadPath\Delete-OfficeLogsAndTraces.ps1"
+Invoke-WebRequest "$githubRepoUrl/Delete-OldTempFiles.ps1" -OutFile "$downloadPath\Delete-OldTempFiles.ps1"
+
+# Create the scheduled task to run the two PowerShell scripts on logon
+$action1 = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$downloadPath\Delete-OfficeLogsAndTraces.ps1 -days 1`""
+$action2 = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$downloadPath\Delete-OldTempFiles.ps1 -days 7`""
+$trigger = New-ScheduledTaskTrigger -AtLogOn -User $currentUserName
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Register-ScheduledTask -TaskName $taskName -Action $action1, $action2 -Trigger $trigger -Settings $settings
